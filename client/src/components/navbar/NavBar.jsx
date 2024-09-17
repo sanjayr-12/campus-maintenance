c2b9d90863ce048f0bcdf9b472c7ac77b7686f28
sanjayr-12 <sanjayr.bit22@gmail.com>
2024-09-17 09:12:35 +0530

    improved UI

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -9,8 +9,8 @@ export const NavBar = () => {
   const user = useStore((state) => state.user);
   const setUser = useStore((state) => state.setUser);
   const setLogout = useStore((state) => state.setLogOut);
-  const data = localStorage.getItem("user")
-  const admin = JSON.parse(data)
+  const data = localStorage.getItem("user");
+  const admin = JSON.parse(data);
 
   const navigate = useNavigate();
 
@@ -27,9 +27,9 @@ export const NavBar = () => {
         navigate("/login");
       }
     } catch (error) {
-      console.log(error);
+      console.error(error.response.data.error);
       setLogout(false);
-      toast.error("error in loggout");
+      toast.error("error in loggout ");
     }
   };
 
@@ -42,7 +42,7 @@ export const NavBar = () => {
       <div className="nav-container">
         <p onClick={handleclick}>{user?.name}</p>
         <Link to="assign-works">Assign Works</Link>
-        {admin.admin && <Link to="register-staff">Register</Link>}
+        {admin?.admin && <Link to="register-staff">Register</Link>}
         <p className="nav-logout" onClick={handleLogout}>
           Logout
         </p>
--- client/src/components/registerStaff/RegisterStaff.jsx
+++ client/src/components/registerStaff/RegisterStaff.jsx
@@ -4,8 +4,9 @@ import "./registerStaff.css";
 import { useEffect, useState } from "react";
 
 export const RegisterStaff = () => {
-    const [staff, setstaff] = useState([]);
-    const [render, setRender] = useState(0)
+  const [staff, setstaff] = useState([]);
+  const [render, setRender] = useState(0);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
     async function getAll() {
@@ -26,6 +27,7 @@ export const RegisterStaff = () => {
   console.log(staff);
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsSubmitting(true);
     const formdata = new FormData(e.target);
     const name = formdata.get("name");
     const password = formdata.get("password");
@@ -35,40 +37,53 @@ export const RegisterStaff = () => {
         "/api/admin/staffSignup",
         { name, password },
         { withCredentials: true }
-        );
-        setRender(render+1)
+      );
+      setRender(render + 1);
       toast.success(response.data.message);
     } catch (error) {
       toast.error(error.response.data.error);
-    } 
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
     <div className="main-register-container">
       <form onSubmit={handleSubmit}>
+        <p>Register for new Staff</p>
         <input type="text" placeholder="Enter the name" name="name" />
         <br />
         <input type="text" placeholder="Password" name="password" />
         <br />
-        <input type="submit" />
-
+        <input
+          type="submit"
+          value={isSubmitting ? "Submitting..." : "Submit"}
+          disabled={isSubmitting}
+        />{" "}
         <Toaster />
       </form>
       <span></span>
 
       <div className="get-staff-container">
         <p>All the staff available</p>
-        <ul>
-          {staff.map((item) => {
-            return (
-              <div key={item._id} className="children">
-                <li className="staffs">
-                  {item.name} - {item.slug}
-                </li>
-              </div>
-            );
-          })}
-        </ul>
+        <table className="staff-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>User ID</th>
+            </tr>
+          </thead>
+          <tbody>
+            {staff.map((item) => {
+              return (
+                <tr key={item._id}>
+                  <td>{item.name}</td>
+                  <td>{item.slug}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
